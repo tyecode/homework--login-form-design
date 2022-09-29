46d831bbdf6bd7c3f174fe492327edb72314bc35
tyecode <sengphachanh.dev@gmail.com>
2022-09-29 10:58:49 +0700

    fix the resize on mobile phone problem

--- src/index.js
+++ src/index.js
@@ -26,3 +26,7 @@ toSignup.addEventListener('click', () => {
         signinForm.classList.add('hide');
     }, 700)
 })
+
+setTimeout(()=>
+   document.querySelector("meta[name=viewport]").setAttribute("content", "height=" + screen.height*0.9 + "px, width=device-width, initial-scale=1.0")
+, 300);
