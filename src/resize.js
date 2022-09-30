937c7f04be496e1eb285b188c860ce7bda11fbcc
tyecode <sengphachanh.dev@gmail.com>
2022-09-30 13:04:01 +0700

    change name

--- src/resize.js
+++ src/resize.js
@@ -16,15 +16,11 @@ window.addEventListener('load', () => {
 
 // viewport units on mobile
 
-let vh = window.innerHeight * 0.01;
-
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
 window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01;
 
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
+});
 
 // fix the mobile keyboard auto resize
 
